ba56e73aa7335a96c1886f71ba52318edcf18832
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-09 17:52:17 -0600

    develop get a post and change response status

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,18 +11,32 @@ class Post(BaseModel):
   published: bool = True
   rating: Optional[int] = None
 
-my_post = []
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+           {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
 
 @app.get("/")
-async def root():
+def root():
   return {"message": "welcome to my API"}
 
 @app.get("/posts")
 def get_posts():
-  return {"data": "This is your posts"}
+  return {"data": my_posts}
 
-@app.post("/posts")
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+  post_found =  next(filter(lambda x: x['id'] == id, my_posts),None)
+  if not post_found:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id } was not found")
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {"message": f"post with id: {id} was not found"}
+
+  return {"data": post_found}
+  
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-  print(post.model_dump_json())
-  return {"data": post}
+  post_dict = post.model_dump(mode="python")
+  post_dict['id'] = randrange(0, 100000)
+  my_posts.append(post_dict)
+  return {"data": post_dict}
