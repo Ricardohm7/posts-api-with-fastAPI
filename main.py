be38b413cc3278d972064ccb0bccf81bd5dc6781
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-12 00:22:09 -0600

    develop delete method completed

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ class Post(BaseModel):
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
+def find_post_index(id: int):
+  for i, post in enumerate(my_posts):
+    if post['id'] == id:
+      return i
+  return None
+
 
 @app.get("/")
 def root():
@@ -31,7 +37,7 @@ def get_post(id: int, response: Response):
     # response.status_code = status.HTTP_404_NOT_FOUND
     # return {"message": f"post with id: {id} was not found"}
 
-  return {"data": post_found}
+  return {"post_detail": post_found}
   
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
@@ -40,3 +46,12 @@ def create_posts(post: Post):
   post_dict['id'] = randrange(0, 100000)
   my_posts.append(post_dict)
   return {"data": post_dict}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+  index = find_post_index(id)
+  if index is None:
+    raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
+  my_posts.pop(index)
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
