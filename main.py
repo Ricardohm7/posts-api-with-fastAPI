42ff5accbf6063da56ecec8298c6dabe06a6a73a
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-12 01:07:50 -0600

    develop update a post feature

--- main.py
+++ main.py
@@ -55,3 +55,16 @@ def delete_post(id: int):
     raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
   my_posts.pop(index)
   return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+  index = find_post_index(id)
+  if index is None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
+  
+  post_dict = post.model_dump(mode="python")
+  post_dict['id'] = id
+  my_posts[index] = post_dict
+  return {"data": post_dict}
+
+  
